5ca98539d853596f3b9e907621390dee3aa28826
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-24 02:01:17 +0000

    character counter now counts down and turns red

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#tweet-text").on("keyup", function(event) {
+    let $tweetTextArea = $(this);
+    let $tweetForm = $tweetTextArea.parent();
+    let $counter = $tweetForm.find(".counter");
+    let numOfChars = $tweetTextArea.val().length;
+    let charsLeft = 140 - numOfChars;
+    $counter.text(charsLeft);
+    if(charsLeft < 0) {
+      $counter.addClass("counter-negative");
+    } else {
+      $counter.removeClass("counter-negative");
+    }
+  })
+});
