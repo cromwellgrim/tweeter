98787f240ee36f5511acded24ee4ec66a62a5b5d
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-26 03:26:22 +0000

    eslinted and prettiered

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper = require("../lib/util/user-helper");
 
-const express       = require('express');
-const tweetsRoutes  = express.Router();
+const express = require("express");
+const tweetsRoutes = express.Router();
 
 module.exports = function(DataHelpers) {
-
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
       if (err) {
@@ -16,20 +15,22 @@ module.exports = function(DataHelpers) {
       }
     });
   });
-  
+
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
+      res.status(400).json({ error: "invalid request: no data in POST body" });
       return;
     }
 
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+    const user = req.body.user
+      ? req.body.user
+      : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
-        text: req.body.text
+        text: req.body.text,
       },
-      created_at: Date.now()
+      created_at: Date.now(),
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
@@ -40,12 +41,14 @@ module.exports = function(DataHelpers) {
       }
     });
   });
+  
   /* receives tweet data and saves it to the db */
   tweetsRoutes.post("/tweets", function(req, res) {
     let freshTweet = req.body.text;
     DataHelpers.saveTweet((freshTweet, err));
-    res.redirect('/');
+    res.redirect("/");
   });
+
   /* sends db info back to the main page to post in a tweet container */
   tweetsRoutes.get("/tweets", function(req, res) {
     DataHelpers.getTweets(db);
@@ -53,5 +56,4 @@ module.exports = function(DataHelpers) {
   });
 
   return tweetsRoutes;
-
-}
+};
