7254d9a94fc47b8e0d7717a829e3cd37a9ac1408
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-03 06:38:26 +0000

    eslint and prettiered again

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,35 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- 
+
 /* security function to prevent hijacking page from tweet form submission */
-const escape = function(str) {
-  const div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
+const escape = function (str) {
+	const div = document.createElement("div");
+	div.appendChild(document.createTextNode(str));
+	return div.innerHTML;
 };
 
 /* change the created_at element to day amount */
-const timeSinceTweet = function(now, before) {
-  const timePassed = now - before;
-  const minute = 60 * 1000;
-  const hour = minute * 60;
-  const day = hour * 60;
+const timeSinceTweet = function (now, before) {
+	const timePassed = now - before;
+	const minute = 60 * 1000;
+	const hour = minute * 60;
+	const day = hour * 60;
 
-  if (timePassed < minute) {
-    return Math.round(timePassed / 1000) + ' seconds ago';
-  } else if (timePassed < hour) {
-    return Math.round(timePassed / minute) + ' minutes ago';
-  } else if (timePassed < day) {
-    return Math.round(timePassed / hour) + ' hours ago';
-  } else if (timePassed > day) {
-    return 'This tweet happened a long time ago';
-  }
-
+	if (timePassed < minute) {
+		return Math.round(timePassed / 1000) + " seconds ago";
+	} else if (timePassed < hour) {
+		return Math.round(timePassed / minute) + " minutes ago";
+	} else if (timePassed < day) {
+		return Math.round(timePassed / hour) + " hours ago";
+	} else if (timePassed > day) {
+		return "This tweet happened a long time ago";
+	}
 };
 
 /* createTweetElement constructs an html article for the tweet based off info delivered from the db */
-const createTweetElement = function(tweet) {
-  const $tweet = $(`
+const createTweetElement = function (tweet) {
+	const $tweet = $(`
   <article class="tweet-article">
     <header class="tweet-container-header">
       <div class="user-pic-name">
@@ -54,104 +53,106 @@ const createTweetElement = function(tweet) {
     </footer>  
   </article>
   `);
-  return $tweet;
+
+	return $tweet;
 };
 
 /* toggle stretch goal 2: sends user back to top of screen */
-$(document).ready(function() {
-  $(".to-the-top").hide();
-  $(window).on("scroll", function(event) {
-    const showArrow = 200;
-    if ($(this).scrollTop() > showArrow) {
-      $(".to-the-top").fadeIn();
-    } else {
-      $(".to-the-top").fadeOut();
-    }
-  });
-  $(".to-the-top").on("click", function(event) {
-    $("html, body").animate({ scrollTop: 0 }, 800);
-    return false;
-  });
+$(document).ready(function () {
+	$(".to-the-top").hide();
+
+	$(window).on("scroll", function (event) {
+		const showArrow = 200;
+		if ($(this).scrollTop() > showArrow) {
+			$(".to-the-top").fadeIn();
+		} else {
+			$(".to-the-top").fadeOut();
+		}
+	});
+
+	$(".to-the-top").on("click", function (event) {
+		$("html, body").animate({ scrollTop: 0 }, 800);
+		return false;
+	});
 });
 
 /* renderTweets loops through my tweet db and inputs the info into the createTweetElement */
-const renderTweets = function(tweets) {
-  for (let tweet of Object.values(tweets)) {
-    $(".tweet-container").prepend(createTweetElement(tweet));
-  }
+const renderTweets = function (tweets) {
+	for (let tweet of Object.values(tweets)) {
+		$(".tweet-container").prepend(createTweetElement(tweet));
+	}
 };
 
 /* stretch: this toggles the new tweet form */
-$(document).ready(function() {
-  $(".nav-bar-btn").on("click", function(event) {
-    $(".error-box").toggle("show");
-    $("#tweet-form").toggle("show");
-    $("#tweet-text").focus();
-  });
+$(document).ready(function () {
+	$(".nav-bar-btn").on("click", function (event) {
+		$(".error-box").toggle("show");
+		$("#tweet-form").toggle("show");
+		$("#tweet-text").focus();
+	});
 });
 
 /* validation function */
-const tweetValid = function(tweetText) {
-  const tweetLength = tweetText.length;
-  const $tweetEmpty = $(`
+const tweetValid = function (tweetText) {
+	const tweetLength = tweetText.length;
+
+	const $tweetEmpty = $(`
     <p id="error"> Let us know your thoughts, type in a tweet</p>
   `);
-  const $tweetLong = $(`
+
+	const $tweetLong = $(`
     <p id="error"> That's too many thoughts, cut your tweet back</p>
   `);
 
-  if (!tweetLength) {
-    $(".error-box").show();
-    $(".error-box").empty();
-    $(".error-box").append($tweetEmpty);
-    return;
-  }
+	if (!tweetLength) {
+		$(".error-box").show();
+		$(".error-box").empty();
+		$(".error-box").append($tweetEmpty);
+		return;
+	}
 
-  if (tweetLength > 140) {
-    $(".error-box").show();
-    $(".error-box").empty();
-    $(".error-box").append($tweetLong);
-    return;
-  }
+	if (tweetLength > 140) {
+		$(".error-box").show();
+		$(".error-box").empty();
+		$(".error-box").append($tweetLong);
+		return;
+	}
 
-  if (140 > tweetLength >= 0) {
-    $(".error-box").hide();
-    return true;
-  }
-
-}
+	if (140 > tweetLength >= 0) {
+		$(".error-box").hide();
+		return true;
+	}
+};
 
 /* Submit event from the tweet form, checks for empty tweets or long tweets and shows errors.
    If no errors it clears my tweet form and sends the serialized data to the /tweets page */
-$(document).ready(function() {
-  $("#tweet-form").on("submit", function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+	$("#tweet-form").on("submit", function (event) {
+		event.preventDefault();
 
-  if (tweetValid($("#tweet-text").val())) {
-    $.ajax({
-      url: "/tweets",
-      method: "POST",
-      data: $("#tweet-text").serialize()
-    }).then(() => {
-      $(".counter").val(140);
-      $("#tweet-text").val("");
-      $("article").remove();
-      getTweets();
-    });
-  }
-})
+		if (tweetValid($("#tweet-text").val())) {
+			$.ajax({
+				url: "/tweets",
+				method: "POST",
+				data: $("#tweet-text").serialize(),
+			}).then(() => {
+				$(".counter").val(140);
+				$("#tweet-text").val("");
+				$("article").remove();
+				getTweets();
+			});
+		}
+	});
 });
 
-
 /* collects newly added info from the db (freshTweet) and renders it to the tweet container */
-const getTweets = function() {
-  $.ajax({
-    url: "/tweets",
-    method: "GET",
-    dataType: "JSON",
-    success: (freshTweet) => {
-      renderTweets(freshTweet);
-    }
-  })
+const getTweets = function () {
+	$.ajax({
+		url: "/tweets",
+		method: "GET",
+		dataType: "JSON",
+		success: (freshTweet) => {
+			renderTweets(freshTweet);
+		},
+	});
 };
-
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,21 +4,19 @@
   140. Adds a class when the count is negative to change the
   font colour to red.
 */
-$(document).ready(function() {
-  $("#tweet-text").on("keyup", function(event) {
-    
-    const $tweetTextArea = $(this);
-    const $tweetForm = $tweetTextArea.parent();
-    const $counter = $tweetForm.find(".counter");
-    const numOfChars = $tweetTextArea.val().length;
-    const charsLeft = 140 - numOfChars;
-    
-    $counter.text(charsLeft);
-    if (charsLeft < 0) {
-      $counter.addClass("counter-negative");
-    } else {
-      $counter.removeClass("counter-negative");
-    }
+$(document).ready(function () {
+	$("#tweet-text").on("keyup", function (event) {
+		const $tweetTextArea = $(this);
+		const $tweetForm = $tweetTextArea.parent();
+		const $counter = $tweetForm.find(".counter");
+		const numOfChars = $tweetTextArea.val().length;
+		const charsLeft = 140 - numOfChars;
 
-  });
+		$counter.text(charsLeft);
+		if (charsLeft < 0) {
+			$counter.addClass("counter-negative");
+		} else {
+			$counter.removeClass("counter-negative");
+		}
+	});
 });
